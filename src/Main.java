58c3fde860467fcaaebf8f3e4db8379ca45ce64d
baonguyen96 <baonguyendrump@gmail.com>
2017-09-06 18:27:50 -0500

    Minor updates

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        String[] l1 = {"C", "Z"};
-        String[] l2 = {"P", "A"};
-        String[] combined;
 
     }
-
 
 }
