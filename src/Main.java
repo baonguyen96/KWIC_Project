878e89555f01dd8765872c5b7d79b2ea5f1466ba
snehachandra <sneha.bitsgoa@gmail.com>
2017-09-13 00:20:28 -0500

    Initial Implementation for Circular shift and Alphabetized Circular shift

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static final String ERROR = "";
 
     public static void main(String[] args) {
+        List<String> shiftedLines = new ArrayList<>();
 
+        ICircularShift shifter = new CircularShift("I GOT THIS");
+        shiftedLines.addAll(shifter.getShiftedLines());
+
+        System.out.println("circular shift");
+        shifter.print();
+
+        shifter=new CircularShift("NOPE NOT THIS");
+        shiftedLines.addAll(shifter.getShiftedLines());
+
+        System.out.println("circular shift");
+        shifter.print();
+
+        IAlphabetizedCircularShift alphabetizer = new AlphabetizedCircularShift(shiftedLines);
+        System.out.println("alphabetised circular shift");
+        alphabetizer.print();
     }
 
 }
